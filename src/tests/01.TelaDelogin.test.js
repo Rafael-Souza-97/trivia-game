7e5fdd68d8158f0c2d409633aae21c0f51c140e8
Marllon Tinoco <tinocodev@gmail.com>
2022-09-09 13:02:50 -0300

    criado arquivo de teste + estrutura básica

---
+++ src/tests/01.TelaDelogin.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+
+describe('Testa a tela de login', () => {
+  it('', () => {
+    
+  })
+})
