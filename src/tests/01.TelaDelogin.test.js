87e3919ca8f289f7eed1d3312029203e241206df
Rafael Souza <souza_rafael.97@outlook.com>
2022-09-12 16:27:45 -0300

    Adiciona requisito 10 e resolve erros no lint

--- src/tests/01.TelaDelogin.test.js
+++ src/tests/01.TelaDelogin.test.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import fetchTrivia from '../services/fetchs/fetchTrivia';
-import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
-import '../App';
-import App from "../App";
+import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+import App from '../App';
+
+const EMAIL = 'trybe@teste.com';
 
 describe('Testa a tela de login', () => {
-
   it('Verifica se a tela de login contém os inputs necessários', () => {
-    
-    renderWithRouterAndRedux(<App />)
+    renderWithRouterAndRedux(<App />);
 
     const btn = screen.getByRole('button', { name: 'Play' });
     const inputName = screen.getByPlaceholderText('Seu Nome');
@@ -20,24 +18,20 @@ describe('Testa a tela de login', () => {
     expect(inputName).toBeInTheDocument();
     expect(inputEmail).toBeInTheDocument();
     expect(btn).toBeInTheDocument();
-
-  })
+  });
 
   it('Verifica se o botão de configuração redireciona para a rota /settings', () => {
-
-    const { history } = renderWithRouterAndRedux(<App />)
+    const { history } = renderWithRouterAndRedux(<App />);
     const btn1 = screen.getByRole('button', { name: 'Configurações' });
-    
+
     userEvent.click(btn1);
 
     const { location: { pathname } } = history;
 
     expect(pathname).toBe('/settings');
-
-  })
+  });
 
   it('Verifica se o botão Play é habilitado quando os inputs são preenchidos', () => {
-    
     renderWithRouterAndRedux(<App />);
 
     const btn = screen.getByRole('button', { name: 'Play' });
@@ -47,14 +41,12 @@ describe('Testa a tela de login', () => {
     expect(btn).toBeDisabled();
 
     userEvent.type(inputName, 'Tinoco');
-    userEvent.type(inputEmail, 'trybe@teste.com')
+    userEvent.type(inputEmail, EMAIL);
 
-    expect(btn).not.toBeDisabled()
-
-  })
+    expect(btn).not.toBeDisabled();
+  });
 
   it('Verifica se ao clicar no botão Play, é redirecionado a rota /game', () => {
-
     const { history } = renderWithRouterAndRedux(<App />);
 
     const btn = screen.getByRole('button', { name: 'Play' });
@@ -62,14 +54,13 @@ describe('Testa a tela de login', () => {
     const inputEmail = screen.getByPlaceholderText('Seu Email');
 
     userEvent.type(inputName, 'Tinoco');
-    userEvent.type(inputEmail, 'trybe@teste.com');
+    userEvent.type(inputEmail, EMAIL);
     userEvent.click(btn);
 
     const { location: { pathname } } = history;
 
-    console.log(pathname)
-
-  })
+    console.log(pathname);
+  });
 
   it('Verifica se a API é chamado ao clicar no botão de Play', async () => {
     renderWithRouterAndRedux(<App />);
@@ -78,11 +69,9 @@ describe('Testa a tela de login', () => {
     const inputEmail = screen.getByPlaceholderText('Seu Email');
 
     userEvent.type(inputName, 'Tinoco');
-    userEvent.type(inputEmail, 'trybe@teste.com');
+    userEvent.type(inputEmail, EMAIL);
     userEvent.click(btn);
 
     await fetchTrivia();
-
   });
-
-})
+});
