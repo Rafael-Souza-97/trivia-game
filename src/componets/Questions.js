f19bed84b3ff102e94bc35240fa102aa96929187
lucasrodrigges <lucas.254262@gmail.com>
2022-09-12 14:27:57 -0300

    resolve requisito 07

--- src/componets/Questions.js
+++ src/componets/Questions.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './Questions.css';
 
 export default class Questions extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasAnswer: false,
+    };
+  }
+
+  handleClick = () => this.setState(({ hasAnswer }) => ({ hasAnswer: !hasAnswer }));
+
   render() {
+    const { hasAnswer } = this.state;
     const { firstResult: { category, question }, wrongAnswers, answers } = this.props;
 
     return (
@@ -14,19 +26,23 @@ export default class Questions extends Component {
 
         <div data-testid="answer-options">
           {answers.map((element, index) => (
-            !wrongAnswers.includes(element) ? (
+            wrongAnswers.includes(element) ? (
               <button
+                className={ hasAnswer && 'wrong-answer' }
                 type="button"
                 key={ index }
-                data-testid="correct-answer"
+                data-testid={ `wrong-answer-${index}` }
+                onClick={ this.handleClick }
               >
                 { element }
               </button>
             ) : (
               <button
+                className={ hasAnswer && 'correct-answer' }
                 type="button"
                 key={ index }
-                data-testid={ `wrong-answer-${index}` }
+                data-testid="correct-answer"
+                onClick={ this.handleClick }
               >
                 { element }
               </button>
